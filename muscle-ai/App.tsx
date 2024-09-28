dde50b00a8dd9f60bda9a4bbb01d5db4e294a380
Erik Schneider <erik.schneider.pacheco@gmail.com>
2024-09-28 15:02:20 -0300

    quick fix imports

--- muscle-ai/App.tsx
+++ muscle-ai/App.tsx
@@ -1,12 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View className='flex items-center justify-center h-screen' >
-      <Text className='p-4 bg-red-400'>Muscle AI</Text>
+    <View className="flex items-center justify-center h-screen">
+      <Text className="p-4 bg-red-400">Muscle AI</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
