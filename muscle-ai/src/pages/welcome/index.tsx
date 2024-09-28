895d270fd239a13417d8048748f398e60f0d013f
Erik Schneider <erik.schneider.pacheco@gmail.com>
2024-09-28 19:44:04 -0300

    atom components added and welcome page

--- muscle-ai/src/pages/welcome/index.tsx
+++ muscle-ai/src/pages/welcome/index.tsx
@@ -1,10 +1,21 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { Button as ButtonAtom } from "@/components/button";
+import tailwind from "twrnc";
 
 export default function WelcomePage() {
+  const onPressStartTraining = () => {
+    alert("Working on the weekend like usual");
+  };
+
   return (
-    <View>
-      <Text>WelcomePage</Text>
+    <View style={tailwind`h-full flex items-center justify-around`}>
+      <Text style={tailwind`text-3xl font-bold`}>Bem vindo ao Muscle AI</Text>
+      <ButtonAtom
+        text="Começe os treinos"
+        onPress={onPressStartTraining}
+        style={tailwind`bg-green-900`}
+      />
     </View>
   );
 }
