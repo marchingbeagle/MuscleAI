8d015346771fb2960bd44aa28d894186e5d9bfb5
Gabriel Rodrigues <gabriell.dorodrigues@gmail.com>
2024-10-13 21:08:01 -0300

    Atualização da Tela Home

--- muscle-ai/src/app/(auth)/(tabs)/dashboard/home/index.tsx
+++ muscle-ai/src/app/(auth)/(tabs)/dashboard/home/index.tsx
@@ -1,21 +1,28 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
-import { Button } from "src/components/Button";
-import { router } from "expo-router";
 import { useClerk } from "@clerk/clerk-expo";
 
 export default function Home() {
   const { signOut } = useClerk();
   return (
-    <View className="flex items-center justify-center h-full">
-      <Text>HomePage</Text>
-      <Button
-        label="Sign out"
-        onPress={async () => {
-          await signOut();
-          router.navigate("/welcome");
-        }}
-      />
+    <View className="p-6">
+      <View className="flex-row items-center gap-2">
+        <View className="h-12 w-12 rounded-full bg-[#2f855a]"/>
+        <Text className="text-base font-bold text-[#2f855a]">
+          Olá! Gabriel Rodrigues
+        </Text>
+      </View>
+
+      <Text className="text-lg font-bold text-[#6b7280] py-4">
+        Opções de Treino
+      </Text>
+
+      <View className="flex-row flex-wrap gap-4">
+        <TouchableOpacity className="bg-[#38a169] rounded-xl h-16 w-full" />
+        <TouchableOpacity className="bg-[#38a169] rounded-xl h-16 w-full" />
+        <TouchableOpacity className="bg-[#38a169] rounded-xl h-16 w-full" />
+        <TouchableOpacity className="bg-[#38a169] rounded-xl h-16 w-full" />
+      </View>
     </View>
   );
 }
