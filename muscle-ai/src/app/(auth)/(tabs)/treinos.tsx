37e01d50b0eac33725a33e47a3cb65cd2922f855
GabrielWDuarte <gabrielwbd03@gmail.com>
2024-10-14 23:01:32 -0300

    Correção erro

--- muscle-ai/src/app/(auth)/(tabs)/treinos.tsx
+++ muscle-ai/src/app/(auth)/(tabs)/treinos.tsx
@@ -1,15 +1,10 @@
 import { View, Text } from "react-native";
-import React, { useState } from "react";
-import { Input } from "src/components/Input";
+import React from "react";
 
 export default function TreinoPage() {
-  const [nome, setNome] = React.useState("")
   return (
     <View className="flex items-center justify-center h-full">
       <Text>Treino</Text>
-      <Input value={nome}
-            setValue={setNome}
-            placeholder="johndoe@example.com"/>
     </View>
   );
 }
