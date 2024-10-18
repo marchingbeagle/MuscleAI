641941933117f5ecbeec305e76a13454b7c4a8cd
Erik Schneider <erik.schneider.pacheco@gmail.com>
2024-10-17 23:37:45 -0300

    layout de cadastro e configurações melhoradas

--- muscle-ai/src/app/(auth)/(tabs)/configuracoes.tsx
+++ muscle-ai/src/app/(auth)/(tabs)/configuracoes.tsx
@@ -1,10 +1,10 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import React from "react";
 import { Button } from "src/components/Button";
 import { useClerk } from "@clerk/clerk-expo";
 import { useRouter } from "expo-router";
 
-export default function configuracoes() {
+export default function Configuracoes() {
   const { signOut } = useClerk();
   const router = useRouter();
 
@@ -14,9 +14,46 @@ export default function configuracoes() {
   };
 
   return (
-    <View>
-      <Text>configuracoes</Text>
-      <Button label="Sair da conta" onPress={handleSignOut} />
-    </View>
+    <ScrollView
+      contentContainerStyle={{
+        flexGrow: 1,
+        padding: 20,
+        backgroundColor: "#f5f5f5",
+      }}
+    >
+      <Text className="mb-5 text-2xl font-bold">Configurações</Text>
+      <View className="flex flex-row items-center justify-between gap-5 mb-5">
+        <Text className="mb-2 text-lg">Perfil</Text>
+        <Button
+          label="Editar Perfil"
+          onPress={() => router.push("/editarPerfil")}
+          className="px-24 text-white bg-green-800 rounded-full h-14 color-white"
+        />
+      </View>
+      <View className="flex flex-row items-center justify-between gap-5 mb-5">
+        <Text className="mb-2 text-lg">Notificações</Text>
+        <Button
+          label="Configurar Notificações"
+          onPress={() => router.push("/configurarNotificacoes")}
+          className="flex-1 text-white bg-green-800 rounded-full h-14 color-white"
+        />
+      </View>
+      <View className="flex flex-row items-center justify-between gap-8 mb-5">
+        <Text className="mb-2 text-lg">Segurança</Text>
+        <Button
+          label="Alterar Senha"
+          onPress={() => router.push("/alterarSenha")}
+          className="flex-1 text-white bg-green-800 rounded-full h-14 color-white"
+        />
+      </View>
+      <View className="flex flex-row items-center mb-5">
+        <Text className="mb-2 text-lg basis-1/2">Conta</Text>
+        <Button
+          label="Sair da conta"
+          className="flex-1 text-white bg-green-800 rounded-full h-14 color-white"
+          onPress={handleSignOut}
+        />
+      </View>
+    </ScrollView>
   );
 }
