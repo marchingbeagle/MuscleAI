9b9d118d3ddf2a8c3def298a1bbc0fa92299b13e
Gabriel Rodrigues <gabriell.dorodrigues@gmail.com>
2024-10-17 23:47:56 -0300

    Somes Fix and Cadastro New Design

--- muscle-ai/src/app/(auth)/cadastro.tsx
+++ muscle-ai/src/app/(auth)/cadastro.tsx
@@ -1,10 +1,35 @@
+import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
-import { View, Text } from "react-native";
+import InputGreen from "src/components/mycomponents/InputGreen.";
 
 export default function AlunosPage() {
+  const [name, setName] = React.useState("");
+  const [metas, setMetas] = React.useState("");
+  const [treinoScript, setTreinoScript] = React.useState("");
+  
   return (
-    <View className="flex-1 bg-white">
-      <Text>Teste</Text>
+    <View className="flex items-center mb-6 p-6">
+
+    <View className="w-32 h-32 rounded-full bg-[#38a169]" />
+    <View className="w-full mb-4">
+      <Text className="text-base">Nome</Text>
+      <InputGreen value={name} setValue={setName} placeholder="John" />
     </View>
+    <View className="w-full mb-4">
+      <Text className="text-base">Peso</Text>
+      <InputGreen value={metas} setValue={setMetas} placeholder="67kg" />
+    </View>
+    <View className="w-full mb-4">
+      <Text className="text-base">Altura</Text>
+      <InputGreen value={metas} setValue={setMetas} placeholder="165cm" />
+    </View>
+    <View className="w-full mb-4">
+      <Text className="text-base">Deficiências do aluno</Text>
+      <InputGreen value={metas} setValue={setMetas} placeholder="Descreva as deficiências do Aluno" />
+    </View>
+
+
+
+  </View>
   );
 }
