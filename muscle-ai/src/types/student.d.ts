47864ed4cc7fb655dbc479a94ee422155d69ea3c
Erik Schneider <erik.schneider.pacheco@gmail.com>
2024-10-30 21:19:29 -0300

    flatlist added concertamo o prisma

--- muscle-ai/src/types/student.d.ts
+++ muscle-ai/src/types/student.d.ts
@@ -4,12 +4,10 @@ export interface Student {
   nm_aluno: string;
   deficiencias_aluno: string | null;
   email_aluno: string;
-  numero_aluno: string;
   genero_aluno: string;
-  nm_personal: string;
-  id_treino: string;
+  id_treino: string | null;
   data_nascimento: Date;
   id_aluno: string;
   metas_aluno: string | null;
-  id_personal: string; // Change this to string
+  id_personal: string;
 }
