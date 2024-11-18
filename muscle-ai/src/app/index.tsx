b673a31e9152e112ce72c07066dcf77825cb19cc
Erik Schneider Pacheco <eriks@MacBook-Air-de-Erik.local>
2024-11-18 01:51:20 -0300

    refatoração e adição de alunos funcionando

--- muscle-ai/src/app/index.tsx
+++ muscle-ai/src/app/index.tsx
@@ -7,32 +7,27 @@ import { useAuth } from "@clerk/clerk-expo";
 import { Redirect } from "expo-router/build/exports";
 
 export default function App() {
-  // Estado para verificar se o aplicativo foi inicializado
   const [isInitialized, setIsInitialized] = useState(false);
   const { isSignedIn } = useAuth();
 
-  // Inicia o Banco de Dados
   useEffect(() => {
     const setup = async () => {
-      await initializeDb(); // Inicializa o banco de dados
-      setIsInitialized(true); // Atualiza o estado para indicar que a inicialização foi concluída
+      await initializeDb();
+      setIsInitialized(true);
     };
     setup();
   }, []);
 
-  // Componente que renderiza ao abrir o APP
   return (
     <SafeAreaProvider>
       <StatusBar barStyle={"dark-content"} />
       {isInitialized ? (
-        // Verifica se está iniciado
         isSignedIn ? (
-          <Redirect href="/home" /> // Redireciona para a página inicial se o usuário estiver autenticado
+          <Redirect href="/home" />
         ) : (
-          <Welcome /> // Exibe a tela de boas-vindas se o usuário não estiver autenticado
+          <Welcome />
         )
       ) : (
-        // Caso não inicie, exibe uma tela de Loading
         <SafeAreaView className="flex items-center justify-center">
           <Text>Loading...</Text>
         </SafeAreaView>
